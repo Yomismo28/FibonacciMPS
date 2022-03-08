de76e83f20817466d5f29ead37449c0c972521aa
Yomismo26 <arlym7b@uma.es>
2022-03-08 23:36:23 +0100

    Implemented tests for 0, 1, 2, 3, 4, 5 and -1.

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,3 +1,67 @@
-public class FibonacciTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FibonacciTest {
+    private Fibonacci fibonacci;
+
+    @BeforeEach
+    public void setup(){
+        fibonacci = new Fibonacci();
+    }
+
+    @AfterEach
+    public void finish(){
+        fibonacci = null;
+    }
+
+    @Test
+    public void should0Return0(){
+        var expectedValue = 0;
+        var obtainedValue = fibonacci.compute(0);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void should1Return1(){
+        var expectedValue = 1;
+        var obtainedValue = fibonacci.compute(1);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void should2Return1(){
+        var expectedValue = 1;
+        var obtainedValue = fibonacci.compute(2);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void should3Return2(){
+        var expectedValue = 1;
+        var obtainedValue = fibonacci.compute(2);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void should4Return3(){
+        var expectedValue = 3;
+        var obtainedValue = fibonacci.compute(4);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void should5Return5(){
+        var expectedValue = 5;
+        var obtainedValue = fibonacci.compute(5);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldNeg1Throw(){
+        assertThrows(RuntimeException.class, ()-> fibonacci.compute(-1));
+    }
 }
